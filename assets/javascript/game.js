9827757327eec89cc88107274781ab52c7b23fdd
Dardin <logandalec@gmail.com>
2018-08-29 18:27:46 -0700

    File initilization added charachters to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    var char1 = {
+        attack: 4,
+        hp: 100,
+        counter: 3,
+        sprite: "path",
+
+    }
+
+    var char2 = {
+        attack: 4,
+        hp: 100,
+        counter: 3,
+        sprite: "path",
+
+    }
+
+    var char3 = {
+        attack: 4,
+        hp: 100,
+        counter: 3,
+        sprite: "path",
+        
+    }
+
+    var char4 = {
+        attack: 4,
+        hp: 100,
+        counter: 3,
+        sprite: "path",
+        
+    }
+
+    var char5 = {
+        attack: 4,
+        hp: 100,
+        counter: 3,
+        sprite: "path",
+        
+    }
+
+    var char6 = {
+        attack: 4,
+        hp: 100,
+        counter: 3,
+        sprite: "path",
+        
+    }
+
+    var game = {
+        player: function () {
+
+        },
+        opponent: function () {
+
+        },
+        attack: function () {
+
+        },
+
+
+    }
+
+
+
+}
